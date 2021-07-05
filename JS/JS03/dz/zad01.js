9e3d985c1f174ede71797a1c3700cc225365693a
Ivan Logožar <ivan.logozar@ecx.io>
2021-07-05 16:25:22 +0200

    dz01.js

--- JS/JS03/dz/zad01.js
+++ JS/JS03/dz/zad01.js
@@ -2,42 +2,44 @@ let dString = '04.01.1954';
 let d1 = new Date(dString);
 let d2 = new Date();
 
-function inmilliSeconds(d1,d2){
+let dateDiff =  {
+inmilliSeconds:function (d1,d2){
     let t1 = d1.getTime();
     let t2 = d2.getTime();
     return parseInt(t2-t1);
-}
-function inSeconds(d1,d2){
+},
+ inSeconds:function(d1,d2){
     let t1 = d1.getTime();
     let t2 = d2.getTime();
     return parseInt((t2-t1)/1000);
-}
-function inDays(d1,d2){
+},
+inDays:function (d1,d2){
     let t1 = d1.getTime();
     let t2 = d2.getTime();
     return parseInt((t2-t1)/(24* 3600 * 1000));
-}
-function inWeeks(d1,d2){
+},
+ inWeeks:function(d1,d2){
     let t1=d1.getTime();
     let t2 = d2.getTime();
     return parseInt((t2 - t1) / (24 * 3600 * 1000 * 7));
-}
-function inMonths (d1,d2){
+},
+inMonths:function(d1,d2){
     let d1Y = d1.getFullYear();
     let d2Y = d2.getFullYear();
     let d1M = d1.getMonth();
     let d2M = d2.getMonth();
     return d2M + 12 * d2Y - (d1M + 12 * d1Y);
-}
-function inYears(d1,d2){
+},
+inYears:function (d1,d2){
     return d2.getFullYear() - d1.getFullYear();
 }
-console.log("Number of years since " + dString + ": " + inYears(d1, d2));
-console.log("Number of months since " + dString + ": " + inMonths(d1, d2));
-console.log("Number of weeks since " + dString + ": " + inWeeks(d1, d2));
-console.log("Number of days since " + dString + ": " + inDays(d1, d2));
-console.log("Number of seconds since " + dString + ": " + inSeconds(d1, d2));
-console.log("Number of milliseconds since " + dString + ": " + inmilliSeconds(d1, d2));
+}
+console.log("Number of years since " + dString + ": " + dateDiff.inYears(d1, d2));
+console.log("Number of months since " + dString + ": " + dateDiff.inMonths(d1, d2));
+console.log("Number of weeks since " + dString + ": " + dateDiff.inWeeks(d1, d2));
+console.log("Number of days since " + dString + ": " + dateDiff.inDays(d1, d2));
+console.log("Number of seconds since " + dString + ": " + dateDiff.inSeconds(d1, d2));
+console.log("Number of milliseconds since " + dString + ": " + dateDiff.inmilliSeconds(d1, d2));
 
 
 
